daf3574f13f8b386dbfb0b66196555494c2ff0f4
victoriaminho <v.moraisminho@gmail.com>
2025-04-27 19:08:49 -0300

    jogo de adivinhação

--- aula_infiny_4_.py
+++ aula_infiny_4_.py
@@ -1,18 +1,18 @@
-# Solicita ao usuário o início e fim do intervalo
-inicio = int(input("Digite o número inicial do intervalo: "))
-fim = int(input("Digite o número final do intervalo: "))
+# Número que o jogador deve adivinhar
+numero_secreto = 7
 
-soma_pares = 0
-encontrou_par = False
+# Contador de tentativas
+tentativas = 0
 
-# Loop que percorre o intervalo (inclusive)
-for num in range(inicio, fim + 1):
-    if num % 2 == 0:
-        soma_pares += num
-        encontrou_par = True
+# Limite de tentativas
+limite_tentativas = 2
+
+while tentativas < limite_tentativas:
+    palpite = int(input("Adivinhe o número (entre 1 e 10): "))
+    tentativas += 1
+
+    if palpite == numero_secreto:
+        print("Parabéns! Você acertou o número!")
+        break
 else:
-    if not encontrou_par:
-        print("Não há números pares no intervalo.")
-
- #Exibe o resultado
-print(f"Soma dos números pares no intervalo: {soma_pares}")
+    print("Que pena! Suas tentativas acabaram. O número era", numero_secreto)
